d67c58d986cfc9b57db00ff08aa691e9939c1b13
KoukiShimoya <shiomyameiji@gmail.com>
2018-11-25 18:36:21 +0900

    last

--- BitWorld/Assets/C#/Flag.cs
+++ BitWorld/Assets/C#/Flag.cs
@@ -50,7 +50,7 @@ public class Flag : MonoBehaviour {
     [SerializeField] Vector3[] AstroPosition = new Vector3[6];
     private bool bodyL1 = false; private bool bodyL2 = false; private bool bodyR1 = false; private bool bodyR2 = false; private bool hasLench = false; bool openPanel = false;
     private bool wingR_edge = false; bool wing_L_corner = false;
-    private InputField inputField;
+    [SerializeField] private InputField inputField;
     private Text text; Text text_01;
     private AudioSource[] sounds;
 
@@ -67,7 +67,7 @@ public class Flag : MonoBehaviour {
         text = GameObject.Find("Text").GetComponent<Text>();
         text_01 = GameObject.Find("Text_01").GetComponent<Text>();
         sounds = GetComponents<AudioSource>();
-        //inputField = GameObject.Find("InputField").GetComponent<InputField>();
+        inputField.gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -137,7 +137,6 @@ public class Flag : MonoBehaviour {
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Debug.DrawRay(ray.origin, ray.direction * 2, Color.red);
             RaycastHit hit = new RaycastHit();
             if (Physics.Raycast(ray, out hit, 1))
             {
@@ -152,7 +151,6 @@ public class Flag : MonoBehaviour {
         text.text = "飛行士：おい、起きろ！緊急事態だ！脱出するぞ！";
         text_01.text = "awake";
         awake_time += Time.deltaTime;
-        Debug.Log(awake_time);
         if(awake_time > 2f)
         {
             state = State.tail;
@@ -200,6 +198,7 @@ public class Flag : MonoBehaviour {
 
     void Charging()
     {
+        sounds[0].PlayOneShot(sounds[0].clip);
         glitch.intensity -= 0.01f;
 
         if(glitch.intensity <= 0)
@@ -212,38 +211,42 @@ public class Flag : MonoBehaviour {
 
     void AfterCharge(GameObject hitObj)
     {
-        text.text = "宇宙飛行士：扉をハックして開けてくれ" +
-            "9つのボタンを Z を描く順で開くはずだ";
+        text.text = "宇宙飛行士：扉をハックして開けてくれ" + "\n" +
+            "右下の9つのボタンを Z を描く順で押せば開くはずだ";
         text_01.text = "afterCharge";
         if (hitObj == null) { return; }
         if(!heapButtons[0].boolean && !heapButtons[1].boolean && !heapButtons[2].boolean && !heapButtons[3].boolean && !heapButtons[4].boolean && !heapButtons[5].boolean && !heapButtons[6].boolean && !heapButtons[7].boolean && !heapButtons[8].boolean)
         {
-            if(hitObj == heapButtons[2].gameObject) { heapButtons[2].boolean = true; }
+            if(hitObj == heapButtons[2].gameObject) { heapButtons[2].boolean = true; sounds[0].PlayOneShot(sounds[0].clip); }
         }
         else if(!heapButtons[0].boolean && !heapButtons[1].boolean && heapButtons[2].boolean && !heapButtons[3].boolean && !heapButtons[4].boolean && !heapButtons[5].boolean && !heapButtons[6].boolean && !heapButtons[7].boolean && !heapButtons[8].boolean)
         {
-            if(heapButtons[1].gameObject == hitObj) { heapButtons[1].boolean = true; }
+            if(heapButtons[1].gameObject == hitObj) { heapButtons[1].boolean = true; sounds[0].PlayOneShot(sounds[0].clip); }
         }
         else if(!heapButtons[0].boolean && heapButtons[1].boolean && heapButtons[2].boolean && !heapButtons[3].boolean && !heapButtons[4].boolean && !heapButtons[5].boolean && !heapButtons[6].boolean && !heapButtons[7].boolean && !heapButtons[8].boolean)
         {
-            if(hitObj == heapButtons[0].gameObject) { heapButtons[0].boolean = true; }
+            if(hitObj == heapButtons[0].gameObject) { heapButtons[0].boolean = true; sounds[0].PlayOneShot(sounds[0].clip); }
         }
         else if(heapButtons[0].boolean && heapButtons[1].boolean && heapButtons[2].boolean && !heapButtons[3].boolean && !heapButtons[4].boolean && !heapButtons[5].boolean && !heapButtons[6].boolean && !heapButtons[7].boolean && !heapButtons[8].boolean)
         {
-            if (hitObj == heapButtons[4].gameObject) { heapButtons[4].boolean = true; }
+            if (hitObj == heapButtons[4].gameObject) { heapButtons[4].boolean = true; sounds[0].PlayOneShot(sounds[0].clip); }
         }
         else if (heapButtons[0].boolean && heapButtons[1].boolean && heapButtons[2].boolean && !heapButtons[3].boolean && heapButtons[4].boolean && !heapButtons[5].boolean && !heapButtons[6].boolean && !heapButtons[7].boolean && !heapButtons[8].boolean)
         {
-            if (hitObj == heapButtons[8].gameObject) { heapButtons[8].boolean = true; }
+            if (hitObj == heapButtons[8].gameObject) { heapButtons[8].boolean = true; sounds[0].PlayOneShot(sounds[0].clip); }
         }
         else if (heapButtons[0].boolean && heapButtons[1].boolean && heapButtons[2].boolean && !heapButtons[3].boolean && heapButtons[4].boolean && !heapButtons[5].boolean && !heapButtons[6].boolean && !heapButtons[7].boolean && heapButtons[8].boolean)
         {
-            if (hitObj == heapButtons[7].gameObject) { heapButtons[7].boolean = true; }
+            if (hitObj == heapButtons[7].gameObject) { heapButtons[7].boolean = true; sounds[0].PlayOneShot(sounds[0].clip); }
         }
         else if (heapButtons[0].boolean && heapButtons[1].boolean && heapButtons[2].boolean && !heapButtons[3].boolean && heapButtons[4].boolean && !heapButtons[5].boolean && !heapButtons[6].boolean && heapButtons[7].boolean && heapButtons[8].boolean)
         {
             if (hitObj == heapButtons[6].gameObject) {
                 heapButtons[6].boolean = true;
+                sounds[1].PlayOneShot(sounds[0].clip);
+                sounds[3].PlayOneShot(sounds[3].clip);
+                sounds[5].Stop();
+                sounds[6].Play();
                 Destroy(GameObject.Find("door_in_heapbody"));
                 Destroy(GameObject.Find("door_side_heapbody"));
                 Destroy(GameObject.Find("door_collider_heapbody"));
@@ -253,7 +256,8 @@ public class Flag : MonoBehaviour {
         }
         else
         {
-            for(int i = 0; i < heapButtons.Length; i++)
+            sounds[2].PlayOneShot(sounds[2].clip);
+            for (int i = 0; i < heapButtons.Length; i++)
             {
                 heapButtons[i].boolean = false;
             }
@@ -263,7 +267,7 @@ public class Flag : MonoBehaviour {
     void ToBody1()
     {
         tobody1_time += Time.deltaTime;
-        text.text = "宇宙飛行士：いいぞ！次は右の翼の太陽光パネルを修理するぞ！";
+        text.text = "宇宙飛行士：いいぞ！船の先端にSOS発信機がある。" + "\n" + "無線室への扉を開けるには、太陽光パネルを修理する必要があるな";
         text_01.text = "tobody1";
         if(tobody1_time > 10f)
         {
@@ -273,22 +277,24 @@ public class Flag : MonoBehaviour {
 
     void Body1(GameObject hitObj)
     {
-        text.text = "宇宙飛行士：扉を開けるから、電源ボックスを押して電気を入れてくれ";
+        text.text = "宇宙飛行士：次は右の翼の太陽光パネルを修理するぞ！" + "\n" + "扉を開けるから、電源ボックスを押して電気を入れてくれ";
         text_01.text = "body1";
         AstroMove(astro.transform.position, AstroPosition[0], State.body1);
         if (hitObj == null) { return; }
         if(hitObj.name == "body_button_R1")
         {
-            Debug.Log("R1");
+            sounds[1].PlayOneShot(sounds[1].clip);
             if (!bodyR1) { bodyR1 = true; }
         }else if(hitObj.name == "body_button_R2")
         {
-            Debug.Log("R2");
-            if(!bodyR2) { bodyR2 = true; }
+            sounds[1].PlayOneShot(sounds[1].clip);
+            if (!bodyR2) { bodyR2 = true; }
         }
 
         if(bodyR1 && bodyR2)
         {
+            sounds[1].PlayOneShot(sounds[1].clip);
+            sounds[3].PlayOneShot(sounds[3].clip);
             Destroy(GameObject.Find("door_in_bodyWingR"));
             Destroy(GameObject.Find("door_side_bodyWingR"));
             Destroy(GameObject.Find("door_collider_bodyWingR"));
@@ -298,24 +304,29 @@ public class Flag : MonoBehaviour {
 
     void Lench(GameObject hitObj)
     {
-        text.text = "宇宙飛行士：いいぞ、ん？　修理用レンチがあるはずだから探してきてくれないか？";
+        text.text = "宇宙飛行士：いいぞ、ん？" + "\n" + "修理用レンチがあるはずだから探してきてくれないか？";
         text_01.text = "lench";
         AstroMove(astro.transform.position, AstroPosition[1], State.lench);
-        if (hitObj == null) { return; }
-        if(hitObj.name == "lench")
-        {
-            Destroy(GameObject.Find("lench"));
-            hasLench = true;
-        }
-
         if (hasLench)
         {
             float dist = Vector3.Distance(astro.transform.position, this.gameObject.transform.position);
-            if(dist < 1f)
+            Debug.Log(dist);
+            if (dist < 1f)
             {
+                sounds[1].PlayOneShot(sounds[1].clip);
+                sounds[6].Stop();
+                sounds[7].Play();
                 state = State.wingR;
             }
         }
+        if (hitObj == null) { return; }
+        if(hitObj.name == "lench")
+        {
+            sounds[0].PlayOneShot(sounds[0].clip);
+            Destroy(GameObject.Find("lench"));
+            hasLench = true;
+        }
+        
     }
 
     void WingR()
@@ -338,20 +349,24 @@ public class Flag : MonoBehaviour {
 
     void Body2(GameObject hitObj)
     {
-        if (hitObj == null) { return; }
-        text.text = "宇宙飛行士： 右のパネルは治ったぞ。同じように左の扉も開けてくれ";
+        text.text = "宇宙飛行士：よし！右のパネルは治ったぞ" + "\n" + "同じように左の扉も開けてくれ";
         text_01.text = "body2";
+        if (hitObj == null) { return; }
         if (hitObj.name == "body_button_L1")
         {
+            sounds[1].PlayOneShot(sounds[1].clip);
             if (!bodyL1) { bodyL1 = true; }
         }
         else if (hitObj.name == "body_button_L2")
         {
+            sounds[1].PlayOneShot(sounds[1].clip);
             if (!bodyL2) { bodyL2 = true; }
         }
 
         if (bodyL1 && bodyL2)
         {
+            sounds[1].PlayOneShot(sounds[1].clip);
+            sounds[3].PlayOneShot(sounds[3].clip);
             Destroy(GameObject.Find("door_in_bodyWingL"));
             Destroy(GameObject.Find("door_side_bodyWingL"));
             Destroy(GameObject.Find("door_collider_bodyWingL"));
@@ -365,21 +380,31 @@ public class Flag : MonoBehaviour {
         AstroMove(astro.transform.position, AstroPosition[4], State.wingL_opening);
         if(state != preState)
         {
-            Destroy(GameObject.Find("wing_jamdoor_L"));
+            sounds[7].Stop();
+            sounds[8].Play();
+            Destroy(GameObject.Find("L_collider"));
         }
+        text.text = "宇宙飛行士：よし、こっちも直してくる";
+        text_01.text = "wingLbefore";
     }
 
     void WingLOpening(GameObject hitObj)
     {
+        text.text = "宇宙飛行士：うわぁぁぁぁぁぁ。窓が開いている！！！" + "\n" + "中のボタンを押して閉めてくれぇぇぇ";
+        text_01.text = "wingLopening";
         if (hitObj == null) { return; }
         if (hitObj.name == "wing_button")
         {
+            sounds[0].PlayOneShot(sounds[0].clip);
+            sounds[3].PlayOneShot(sounds[3].clip);
             state = State.wingL_after;
         }
     }
 
     void WingLAfter()
     {
+        text.text = "宇宙飛行士：助かった。" + "\n" + "あとは俺が作業するから、外で待っていてくれ";
+        text_01.text = "wingLafter";
         if (!wing_L_corner)
         {
             AstroMove(astro.transform.position, AstroPosition[3], State.wingL_after);
@@ -396,9 +421,13 @@ public class Flag : MonoBehaviour {
 
     void Body3()
     {
+        text.text = "宇宙飛行士：よし、無線室まで来たな" + "\n" + "ここは生体認証だから、俺が開けるぞ";
         door_waitTime += Time.deltaTime;
         if(door_waitTime > 5f)
         {
+            sounds[3].PlayOneShot(sounds[3].clip);
+            sounds[8].Stop();
+            sounds[9].Play();
             Destroy(GameObject.Find("door_in_bodyhead"));
             Destroy(GameObject.Find("door_side_bodyhead"));
             Destroy(GameObject.Find("door_collider_bodyhead"));
@@ -408,18 +437,23 @@ public class Flag : MonoBehaviour {
 
     void Head(GameObject hitObj)
     {
-        if(hitObj.name == "panel")
-        {
-            openPanel = true;
-        }
+        text.text = "宇宙飛行士：よし、最後だ。" + "\n" + "SOSを打ってくれ";
         if (openPanel)
         {
-            
-            if(inputField.text == "010100110100111101010011")
+
+            if (inputField.text == "010100110100111101010011")
             {
                 state = State.finish;
             }
         }
+        if (hitObj == null) { return; }
+        if(hitObj.name == "panel")
+        {
+            sounds[0].PlayOneShot(sounds[0].clip);
+            inputField.gameObject.SetActive(true);
+            openPanel = true;
+        }
+        
     }
 
     void AstroMove(Vector3 from, Vector3 to, State nextState)
@@ -443,11 +477,19 @@ public class Flag : MonoBehaviour {
     void OnTriggerEnter(Collider collider)
     {
         colliderObj = collider.gameObject;
+        if(collider.name == "Ljammer" || collider.name == "Rjammer")
+        {
+            glitch.intensity = 0.5f;
+        }
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider collider)
     {
         colliderObj = null;
+        if (collider.name == "Ljammer" || collider.name == "Rjammer")
+        {
+            glitch.intensity = 0f;
+        }
     }
 
     void collReset()
